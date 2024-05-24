54d4640b815db12686c86f1783e9411e0a7ce799
StartTheTrip <skyeatsspecialk@gmail.com>
2024-05-24 21:27:24 +0100

    Add GetFolder

---
+++ GetFolder/GetFolder.ps1
@@ -0,0 +1,20 @@
+﻿Function Get-Folder($Location="")
+
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "MyComputer"
+    $foldername.SelectedPath = $Location
+
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+
+
+}
+
+Get-Folder
